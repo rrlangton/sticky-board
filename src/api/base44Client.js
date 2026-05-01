aacd8e454b091e31da78d026151fde38d36e7159
Robin <rrlangton@gmail.com>
2026-05-01 14:29:32 -0500

    Deploy app to gh-pages root

--- src/api/base44Client.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@base44/sdk';
-
-export const base44 = createClient({
-  appId: "69f3c6c5fd099f43af28a5cf",
-  headers: {
-    "api_key": "87ba515adf3f4885b1bf201f02e5c268"
-  }
-});
